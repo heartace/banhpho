1086ca0d68b3df56e78fa8c29e1861b3bc548b00
heartace <love@heartace.me>
2015-12-24 19:35:53 +0800

    code refactoring

--- lib/banhpho/cli.rb
+++ lib/banhpho/cli.rb
@@ -1,25 +1,37 @@
 require "thor"
 require 'colorize'
+require 'progress_bar'
 
 module Banhpho
 	class CLI < Thor
 
 		desc "compress DIR", "compress all png files in DIR"
 		def compress(dir)
-			@count = 0
-			@originalSize = 0
-			@newSize = 0
-
 			abort("Error: command pngquant is not found, please download it from https://pngquant.org/ and put it in your global path") unless command?("pngquant")
 			abort("Error: argument '#{dir}' is not a valid directory") unless File.directory?(dir)
 
-			compressInDir(dir)
-			puts("=" * 20)
-			msg = "#{@count} files compressed!"
-			color = @count == 0 ? :red : :blue
+			originalSize = 0
+			newSize = 0
+
+			imgs = []
+			compressInDir(dir) do |f|
+				imgs << f
+				originalSize += File.size(f)
+			end
+			bar = ProgressBar.new(imgs.size)
+			imgs.each do |f|
+				%x(pngquant --force --ext .png --skip-if-larger -- #{f})
+				bar.increment!
+				newSize += File.size(f)
+			end
+
+			count = imgs.size
+			color = count == 0 ? :red : :green
+			puts ("=" * 20).colorize(color)
+			msg = "#{count} files compressed!"
 			puts msg.colorize(color)
-			if @originalSize > 0 then
-				puts("original size: #{prettyFileSize(@originalSize)}, after compression: #{prettyFileSize(@newSize)}, compression ratio: #{(1 - @newSize.to_f / @originalSize).round(4) * 100}%".colorize(:blue))
+			if originalSize > 0 then
+				puts("original size: #{prettyFileSize(originalSize)}, after compression: #{prettyFileSize(newSize)}, compression ratio: #{(1 - newSize.to_f / originalSize).round(4) * 100}%".colorize(color))
 			end
 		end
 
@@ -35,18 +47,13 @@ module Banhpho
 					next if item == "." or item == ".."
 					path = File.join(dir, item)
 					if File.directory?(path)
-						puts("checking directory: #{path}")
 						compressInDir(path)
 						next
 					end
 					ext = File.extname(path).downcase
 					if ext == ".png" then
-						@originalSize += File.size(path)
-						exePath = path.gsub(/ /, "\\ ")
-						puts("  compressing file: #{path}")
-						%x(pngquant --force --ext .png --skip-if-larger -- #{exePath})
-						@count += 1
-						@newSize += File.size(path)
+						fixedPath = path.gsub(/ /, "\\ ")
+						yield fixedPath 
 					end
 				end
 			end
